2cbfba0251186cd41e1140aa8db8ca9c2ae2cc14
krischal07 <krischal.shrestha9849@gmail.com>
2024-08-16 14:39:15 +0545

    Calculation done

--- calculate_bac.php
+++ calculate_bac.php
@@ -1,33 +1,54 @@
 <?php
 // echo "Hello this is calucationbac ";
 
-$weight = $_GET["weight"];
-$weight_unit = $_GET['unit'];
-$gender = $_GET['gender'];
-$num_drinks = $_GET['drinks'];
-$alc_content = $_GET['alcohol_content'];
-$time_elapsed = $_GET['time_elapsed'];
-$gender_constant;
-
-if ($weight_unit == 'kg') {
-    $weight = $weight * 2.20462;
+function weight_convert($weight, $weight_unit)
+{
+    if ($weight_unit == 'kg') {
+        return $weight * 2.20462;
+    } else {
+        return $weight;
+    }
 }
 
-if ($gender == 'male') {
-    $gender_constant = 0.73;
-} else {
-    $gender_constant = 0.66;
+function gender_constant_convert($gender)
+{
+    if ($gender == 'male') {
+        return 0.73;
+    } else {
+        return 0.66;
+    }
 }
+
+$weight = $_POST["weight"];
+$weight_unit = $_POST['unit'];
+$gender = $_POST['gender'];
+$num_drinks = $_POST['drinks'];
+$alc_content = $_POST['alcohol_content'];
+$time_elapsed = $_POST['time_elapsed'];
+
+$gender_constant = gender_constant_convert($gender);
+$check_weight = weight_convert($weight, $weight_unit);
+
+
 
 
 echo "the weight is $weight ";
-echo $weight_unit;
+echo '<br>';
+echo "The weght unit is $weight_unit";
+echo '<br>';
 echo $gender;
+echo '<br>';
 echo $num_drinks;
+echo '<br>';
 echo $alc_content;
+echo '<br>';
 echo $time_elapsed;
-echo $gender;
-// $bac = (($num_drinks * 5.14) / ($weight * $gender_constant)) - (0.015 * $time_elapsed);
+echo '<br>';
+echo $gender_constant;
+echo '<br>';
+echo $check_weight;
+
+$bac = (($num_drinks * 5.14) / ($weight * $gender_constant)) - (0.015 * $time_elapsed);
 
 if ($bac > 0.08) {
     echo 'Unsafe to drive';
